4674b1791e32f03b7096db3f5d0dd6c5007b21b0
Bruce Apos <bobapos420@gmail.com>
2023-04-09 18:17:14 +0800

    Complete functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Container, Box } from "@chakra-ui/react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import TextInput from "./components/TextInput";
+import { useState } from "react";
+import KeywordsModal from "./components/KeywordsModal";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [keywords, setKeywords] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const extractKeywords = async (text) => {
+    setIsLoading(true);
+    setIsOpen(true);
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: "text-davinci-003",
+        prompt:
+          "Extract keywords from this text. Make the first letter of each word uppercase and separate with commas\n\n" +
+          text +
+          "",
+        temperature: 0.5,
+        max_tokens: 60,
+        frequency_penalty: 0.8,
+      }),
+    };
+
+    const response = await fetch(import.meta.env.VITE_OPENAI_API_URL, options);
+    const json = await response.json();
+    const data = json.choices[0].text.trim();
+
+    console.log(data);
+    setKeywords(data);
+    setIsLoading(false);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <Box bg="blue.600" color="white" height="100vh" paddingTop={130}>
+      <Container maxW="3xl" centerContent>
+        <Header />
+        <TextInput extractKeywords={extractKeywords} />
+        <Footer />
+      </Container>
+      <KeywordsModal
+        keywords={keywords}
+        loading={isLoading}
+        isOpen={isOpen}
+        closeModal={closeModal}
+      />
+    </Box>
+  );
+};
 
-export default App
+export default App;
